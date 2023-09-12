3909bd05f629d03f34ac32431114b247929807ae
duncan4123 <dmgwizz@gmail.com>
2023-09-12 23:28:38 +1000

    add contracts set chain id

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   extends: ["plugin:prettier/recommended", "prettier/react", "prettier/@typescript-eslint"],
   plugins: ["prettier", "react", "react-hooks"],
   rules: {
-    "prettier/prettier": ["error"],
+    "prettier/prettier": 0,
     "import/prefer-default-export": "off",
     "prefer-destructuring": "off",
     "prefer-template": "off",
