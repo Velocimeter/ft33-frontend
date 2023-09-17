4a4ace6f1eb19493024515b94514c081d6d7ee6d
duncan4123 <dmgwizz@gmail.com>
2023-09-17 23:53:31 +1000

    change from 2200 to 28800

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const THE_GRAPH_URL = "https://api.studio.thegraph.com/query/53020/ft33/version/latest";
-export const EPOCH_INTERVAL = 2200;
+export const EPOCH_INTERVAL = 28800; // from hardhat config in contracts
 
 // NOTE could get this from an outside source since it changes slightly over time
 export const BLOCK_RATE_SECONDS = 13.14;
